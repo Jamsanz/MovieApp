4bbc28cbbab4328b196dd809acc6a092ca9cdf1b
Muhammad Sani <jamiludeensani@gmail.com>
2021-08-19 12:28:03 +0100

    MovieApp v3.0.1

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -10,8 +10,8 @@ import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+// import IconButton from '@material-ui/core/IconButton';
+// import MenuIcon from '@material-ui/icons/Menu';
 import Search from './search';
 import axios from "axios";
 import Favorites from './Favorites'
